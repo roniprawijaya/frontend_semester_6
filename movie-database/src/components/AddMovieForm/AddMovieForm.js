378f507976b545eebe81aae6fb1d7fc53ac3fcea
roniprawijaya <72784388+roniprawijaya@users.noreply.github.com>
2023-03-31 07:08:34 +0700

    pertemuan-5

---
+++ movie-database/src/components/AddMovieForm/AddMovieForm.js
@@ -0,0 +1,54 @@
+import styles from "./AddMovieForm.module.css";
+
+function AddMovieForm() {
+
+  return (
+    <div className={styles.container}>
+      <section className={styles.form}>
+        <div className={styles.form__left}>
+          <img
+            className={styles.form__image}
+            src="https://picsum.photos/536/354"
+            alt=""
+          />
+        </div>
+        <div className={styles.form__right}>
+          <h2 className={styles.form__title}>Add Movie Form</h2>
+          <form >
+            <div className={styles.form__group}>
+              <label htmlFor="title" className={styles.form__label}>
+                Title
+              </label>
+              <input
+                id="title"
+                className={styles.form__input}
+                type="text"
+                name="title"
+              />
+            </div>
+            <div className={styles.form__group}>
+              <label htmlFor="date" className={styles.form__label}>
+                Date
+              </label>
+              <input
+                id="date"
+                className={styles.form__input}
+                type="text"
+                name="date"
+              />
+              {/*
+               * Menambahkan infline if: operator &&
+               * Jika isDateError true maka render error
+               */}
+            </div>
+            <div>
+              <button className={styles.form__button}>Add Movie</button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default AddMovieForm;
