8cb931da2bd338cf80446fab5b8cc9938af7b8cf
roniprawijaya <72784388+roniprawijaya@users.noreply.github.com>
2023-05-30 23:26:49 +0700

    Tugas FE-9

---
+++ movie-database/src/components/AddMovieForm/AddMovieForm.js
@@ -0,0 +1,170 @@
+import { nanoid } from "nanoid";
+import { useState } from "react";
+import Alert from "../Alert/Alert";
+import styles from "./AddMovieForm.module.css";
+
+// Menangkap props
+function AddMovieForm(props) {
+  // Destructing props: state movies
+  const { movies, setMovies } = props;
+
+  const [formData, setFormData] = useState({
+    title: "",
+    date: "",
+    poster: "",
+    type: "",
+  });
+
+  /**
+   * TODO
+   * - PROBLEM: 1 ERROR 1 STATE.
+   * - TODO: REFACTOR SEMUA ERROR JADI 1 STATE.
+   */
+  const [isError, setIsError] = useState([]);
+
+  function handleChange(e) {
+    // Destructing name dan value.
+    const { name, value } = e.target;
+
+    /**
+     * Mengupdate state berupa object:
+     * - Menggunakan spread operator:
+     * - Update property berdasarkan apapun nilai name.
+     */
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+  function validate() {
+    let err = []
+    Object.entries(formData).forEach(([prop, val]) => {
+        if (!val) {
+            err.push(prop)
+        } else {
+            err.filter(item => item !== prop);
+        }
+    })
+    setIsError(err)
+  }
+
+  function addMovie() {
+    const movie = {
+      id: nanoid(),
+      title: title,
+      year: date,
+      type: type,
+      poster: poster,
+    };
+
+    // SOLVED: HOW TO ADD MOVIE TO MOVIES :)
+    setMovies([...movies, movie]);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    validate();
+    if (Object.values(formData).every(Boolean)) {
+        addMovie()
+    }
+  }
+
+  const { title, date, poster, type } = formData;
+
+  return (
+    <div className={styles.container}>
+      <section className={styles.form}>
+        <div className={styles.form__left}>
+          <img
+            className={styles.form__image}
+            src="https://picsum.photos/536/354"
+            alt=""
+          />
+        </div>
+        <div className={styles.form__right}>
+          <h2 className={styles.form__title}>Add Movie Form</h2>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.form__group}>
+              <label htmlFor="title" className={styles.form__label}>
+                Title
+              </label>
+              <input
+                id="title"
+                className={styles.form__input}
+                type="text"
+                name="title"
+                // Memberikan value input: title
+                value={title}
+                // Memberikan event onChange
+                onChange={handleChange}
+              />
+              {/*
+               * Menambahkan infline if: operator &&
+               * Jika isTitleError true maka render error
+               */}
+              {isError.includes('title') && <Alert>Title Wajib Diisi</Alert>}
+            </div>
+            <div className={styles.form__group}>
+              <label htmlFor="date" className={styles.form__label}>
+                Date
+              </label>
+              <input
+                id="date"
+                className={styles.form__input}
+                type="text"
+                name="date"
+                // Memberikan value input: date
+                value={date}
+                // Memberikan event onChange
+                onChange={handleChange}
+              />
+              {/*
+               * Menambahkan infline if: operator &&
+               * Jika isDateError true maka render error
+               */}
+              {isError.includes('date') && <Alert>Date Wajib Diisi</Alert>}
+            </div>
+            <div className={styles.form__group}>
+              <label htmlFor="poster" className={styles.form__label}>
+                Poster
+              </label>
+              <input
+                onChange={handleChange}
+                id="poster"
+                className={styles.form__input}
+                name="poster"
+                type="text"
+                value={poster}
+              />
+              {isError.includes('poster') && <Alert>Poster Wajib Diisi</Alert>}
+            </div>
+            <div className={styles.form__group}>
+              <label htmlFor="type" className={styles.form__label}>
+                Type
+              </label>
+              <select
+                id="type"
+                className={styles.form__select}
+                name="type"
+                value={type}
+                onChange={handleChange}
+              >
+                <option value="Action">Action</option>
+                <option value="Drama">Drama</option>
+                <option value="Horor">Horor</option>
+              </select>
+              {isError.includes('type') && <Alert>Poster Wajib Diisi</Alert>}
+            </div>
+            <div>
+              <button type='submit' className={styles.form__button}>Add Movie</button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default AddMovieForm;
